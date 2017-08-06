ac0d0ce46bfaa968cfdba34430bd30144df900b1
JAYASELVAN  A  K <jayaselvanak@gmail.com>
2017-08-06 12:20:55 +0530

    Update SlidingInversion.java

--- SlidingInversion.java
+++ SlidingInversion.java
@@ -10,14 +10,27 @@ public class Solution {
         // Complete this function
         int b[]=new int[m];
         int c[]=new int[m];
+        int d[]=new int [m];
         int invCount=0;
         for(int i=0;i<=(n-m);i++){
+            int k1=m-1;
+            LinkedHashSet<Integer> lh=new LinkedHashSet<Integer>();
             for(int j=0;j<m;j++){
                 b[j]=a[i+j];
                 c[j]=b[j];
+                d[k1]=b[j];
+                k1--;
+                lh.add(b[j]);
             }
+            k1=m-1;
             Arrays.sort(c);
-            if(!(Arrays.equals(b,c))){
+            if(Arrays.equals(b,d)&&lh.size()==m){
+                while(k1!=0){
+                    invCount+=k1;
+                    k1--;
+                }
+            }
+            else if(!(Arrays.equals(b,c))){
                 for(int j=0;j<(m-1);j++){
                     for(int k=j+1;k<m;k++){
                         if(b[j]>b[k]){
@@ -25,7 +38,7 @@ public class Solution {
                         }
                     }
                 }
-           }   
+            }   
         }
         return invCount;
     }
